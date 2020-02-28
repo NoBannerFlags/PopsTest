848e93206dccaba1dd6c075be0699e9fc375fb69
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2020-02-28 10:31:44 -0700

    Changes to documentation
    Documentation changed to better reflect project

--- PopsProj/Pop.java
+++ PopsProj/Pop.java
@@ -24,14 +24,14 @@ public class Pop
     /**
      * Pop Constructor
      *
-     * @param title title - name of the pop, defaults to "Founders".
-     * @param population population - number of units in the pop.
-     * @param money money - ammount of money units the population has. Defaults to 0.
-     * @param needsCost needsCost - yearly cost of pop needs.
-     * @param families families - number of families in pop.
-     * @param elderly elderly - number of elderly units in pop. Defaults to 0.
-     * @param income income - yearly money gain. Increases int money.
-     * @param employed employed - number of employed units in pop.
+     * @param title name of the pop, defaults to "Founders".
+     * @param population number of units in the pop.
+     * @param money ammount of money units the population has. Defaults to 0.
+     * @param needsCost yearly cost of pop needs.
+     * @param families number of families in pop.
+     * @param elderly number of elderly units in pop. Defaults to 0.
+     * @param income yearly money gain. Increases int money.
+     * @param employed number of employed units in pop.
      */
     public Pop(String title, int population, int money, int needsCost, int families, int elderly, int income, int employed)
     {
@@ -45,6 +45,11 @@ public class Pop
        this.unemployed=unemployed;
     }
 
+    /**
+     * Method toString - returns values as a string
+     *
+     * @return All values of the pop put into text
+     */
     public String toString(){
         return "This population is titled "+this.title+", money:"+this.money+"$, population:"+this.population+" units, needs:"+this.needsCost+"$, has "+families+" families, earns "+this.income+"$ over "+this.employed+" jobs";
     }
